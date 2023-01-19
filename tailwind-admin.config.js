d323271c35b14bfb1274548f9462b6385c49d6c1
realslimsutton <87341175+realslimsutton@users.noreply.github.com>
2023-01-19 17:57:10 +0000

    - Added default config files - Build process for assets - Register custom theme for admin panel

---
+++ tailwind-admin.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/filament/**/*.blade.php'
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
+            }
+        }
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography')
+    ]
+};
