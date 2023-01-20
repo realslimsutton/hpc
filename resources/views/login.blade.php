92adf006cb3e2166db892d20ad4efe2d9f6fc7d0
realslimsutton <87341175+realslimsutton@users.noreply.github.com>
2023-01-20 05:37:35 +0000

    More testing

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,10 +2,10 @@
 
 @section('body')
     <section
-        class="relative py-[135px] before:absolute before:inset-0 before:bg-hpc-red/80"
-        style="background: url('{{ asset_version('images/page-banner.jpg') }}') no-repeat scroll center top transparent"
+        class="relative py-[135px] bg-cover bg-center md:bg-top bg-no-repeat before:absolute before:inset-0 before:bg-hpc-red/80"
+        style="background-image: url('{{ asset_version('images/page-banner.jpg') }}')"
     >
-        <div class="absolute h-[100px] bottom-0 inset-x-0 bg-[linear-gradient(to_top,rgba(90,4,16,1),rgba(90,4,16,0))]"></div>
+        <div class="absolute h-[300px] md:h-[100px] bottom-0 inset-x-0 bg-[linear-gradient(to_top,rgba(90,4,16,1),rgba(90,4,16,0))]"></div>
 
         <div class="flex items-center justify-center relative">
             <h1 class="text-7xl text-white font-bold">
