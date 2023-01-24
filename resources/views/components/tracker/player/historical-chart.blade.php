ecd39fbc997ee135fafb751c27a05889d616ad20
realslimsutton <87341175+realslimsutton@users.noreply.github.com>
2023-01-24 02:42:23 +0000

    Test new layout

--- resources/views/components/tracker/player/historical-chart.blade.php
+++ resources/views/components/tracker/player/historical-chart.blade.php
@@ -3,7 +3,7 @@
 ])
 
 <div
-    class="relative bg-hpc-red-700  rounded-xl border border-hpc-red-800 p-4S"
+    class="relative bg-hpc-red-700  rounded-xl border border-hpc-red-800"
     x-data="{
         data: @js($chartData['data']),
         filters: {
--- resources/views/tracker/player.blade.php
+++ resources/views/tracker/player.blade.php
@@ -104,11 +104,35 @@
         </div>
 
         <div class="grid lg:grid-cols-2 gap-12">
-            @livewire('tracker.player-session-history', ['player' => $player])
-
             <div>
                 <x-tracker.player.historical-chart :chart-data="$chartData"/>
             </div>
+
+            <div class="space-y-12">
+                <div class="w-full space-y-4">
+                    <h2 class="text-4xl font-bold text-white">
+                        Breakdown by Livestream
+                    </h2>
+
+                    <div class="relative bg-hpc-red-700  rounded-xl border border-hpc-red-800 p-4 h-48"></div>
+                </div>
+
+                <div class="w-full space-y-4">
+                    <h2 class="text-4xl font-bold text-white">
+                        Custom Breakdown
+                    </h2>
+
+                    <div class="relative bg-hpc-red-700  rounded-xl border border-hpc-red-800 p-4 h-48"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full space-y-4">
+            <h2 class="text-4xl font-bold text-white">
+                Session History
+            </h2>
+
+            @livewire('tracker.player-session-history', ['player' => $player])
         </div>
     </div>
 @endsection
