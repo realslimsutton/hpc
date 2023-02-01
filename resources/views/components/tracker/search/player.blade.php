5d49e4e6f6f68182cbe6a46a6a93bbc6dba4aa8a
realslimsutton <87341175+realslimsutton@users.noreply.github.com>
2023-02-01 13:44:33 +0000

    - Fix tracker search

--- resources/views/components/tracker/search/player.blade.php
+++ resources/views/components/tracker/search/player.blade.php
@@ -3,33 +3,27 @@
 ])
 
 <a
-    href="#"
+    href="{{ route('tracker.player', $result->id) }}"
     class="w-full py-4 px-6 h-20 flex items-center gap-4"
-    x-data
 >
-    @if($result->location->featured_image !== null)
-        <div>
-            <x-curator-glider :media="$result->location->featured_image" class="h-16 w-auto rounded-full"/>
-        </div>
-    @endif
+    <div>
+        @if($result->featured_image !== null)
+            <x-curator-glider :media="$result->featured_image" class="h-16 w-16 rounded-full"/>
+        @else
+            <img
+                src="https://ui-avatars.com/api/?name={{ urlencode($result->name) }}&color=FFCD67&background=5A0410"
+                class="h-16 w-16 rounded-full"
+            />
+        @endif
+    </div>
 
-    <div class="flex-grow flex justify-between flex-wrap">
-        <div>
-            <h3 class="font-medium text-lg">
-                {{ $result->name }}
-            </h3>
+    <div>
+        <h3 class="font-medium text-lg">
+            {{ $result->name }}
+        </h3>
 
-            <p class="text-hpc-gold text-sm font-medium">
-                Livestream
-            </p>
-        </div>
-
-        <x-button
-            class="text-sm"
-            size="sm"
-            x-on:click.prevent="window.open('{{ $result->stream_url }}', '_blank').focus()"
-        >
-            View stream
-        </x-button>
+        <p class="text-hpc-gold text-sm font-medium">
+            {{ $result->profession ?? 'Player' }}
+        </p>
     </div>
 </a>
